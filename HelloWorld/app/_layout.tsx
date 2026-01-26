c00cb4520feb5570c51927fddbef6d475b643f7c
Ken Thampiratwong <ken.thampiratwong@gmail.com>
2026-01-25 17:27:55 -0800

    add: nativewind

--- HelloWorld/app/_layout.tsx
+++ HelloWorld/app/_layout.tsx
@@ -1,13 +1,15 @@
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 
+import "@/global.css"
+
 export default function RootLayout() {
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen
+      <Stack.Screen
         name="modal" options={{ presentation: 'modal', title: 'Modal' }}
-        />
+      />
     </Stack>
   );
 }
