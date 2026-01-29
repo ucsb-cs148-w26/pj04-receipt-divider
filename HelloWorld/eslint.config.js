b858250a8b9717e5a1ca902877a73e66fe31563f
Leifeng Chen <leifengchen@ucsb.edu>
2026-01-28 21:56:35 -0800

    fix: eslint.config enable prettier

--- HelloWorld/eslint.config.js
+++ HelloWorld/eslint.config.js
@@ -5,7 +5,7 @@ const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommen
 
 module.exports = defineConfig([
   expoConfig,
-  //eslintPluginPrettierRecommended,
+  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*']
   },
