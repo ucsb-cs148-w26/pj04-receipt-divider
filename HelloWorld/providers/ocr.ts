13736842905ef9d967d4ede7d35233d4ed1baf7e
Ken Thampiratwong <ken.thampiratwong@gmail.com>
2026-01-30 15:58:51 -0800

    dev: refactor orc.js to typescript

---
+++ HelloWorld/providers/ocr.ts
@@ -0,0 +1,36 @@
+const analyzeReceipt = async (base64Image: Base64URLString): Promise<string[]> => {
+  if (!base64Image) return [];
+
+  try {
+    const url = new URL(process.env.EXPO_PUBLIC_GOOGLE_URL as string);
+    url.searchParams.set("key", process.env.EXPO_PUBLIC_GOOGLE_API_KEY as string);
+
+    const body = {
+      requests: [{
+        image: { content: base64Image },
+        features: [{ type: "TEXT_DETECTION" }],
+      }],
+    };
+
+    const response = await fetch(process.env.EXPO_PUBLIC_GOOGLE_URL as string, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (!data.responses || !data.responses[0].textAnnotations) {
+      return [];
+    }
+
+    const fullText = data.responses[0].textAnnotations[0].description;
+    return fullText.split('\n');
+
+  } catch (error) {
+    console.error("API Error:", error);
+    return [];
+  }
+};
+
+export default analyzeReceipt;
