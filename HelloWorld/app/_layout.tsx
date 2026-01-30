07b1194ed31adce71f24488cf8a33a3fb261fa41
Edward Garcia <edwardgarcia@ucsb.edu>
2026-01-30 07:59:59 -0800

    fix: items no longer removed when navigating pages

--- HelloWorld/app/_layout.tsx
+++ HelloWorld/app/_layout.tsx
@@ -2,14 +2,17 @@ import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 
 import '@/global.css';
+import { ReceiptProvider } from '../contexts/ReceiptContext';
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name='modal'
-        options={{ presentation: 'modal', title: 'Modal' }}
-      />
-    </Stack>
+    <ReceiptProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name='modal'
+          options={{ presentation: 'modal', title: 'Modal' }}
+        />
+      </Stack>
+    </ReceiptProvider>
   );
 }
