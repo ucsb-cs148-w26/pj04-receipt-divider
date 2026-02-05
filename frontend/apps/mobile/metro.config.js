c917bb20fec017daffc7f2d567c1860604dfdb42
Ken Thampiratwong <ken.thampiratwong@gmail.com>
2026-02-05 14:24:23 -0800

    chore: format

--- frontend/apps/mobile/metro.config.js
+++ frontend/apps/mobile/metro.config.js
@@ -4,10 +4,7 @@ const path = require('path');
 let config = getDefaultConfig(__dirname);
 
 config.resolver.sourceExts = ['ts', 'tsx', 'js', 'jsx', 'json'];
-config.watchFolders = [
-  __dirname,
-  path.resolve(__dirname, '../../shared')
-];
-config = withNativeWind(config, { input: "./global.css" })
+config.watchFolders = [__dirname, path.resolve(__dirname, '../../shared')];
+config = withNativeWind(config, { input: './global.css' });
 
 module.exports = config;
