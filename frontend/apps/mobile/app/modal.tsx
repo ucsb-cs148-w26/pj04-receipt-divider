e1f78a6bc9f62232e827f249af3567a0c3691b58
Ken Thampiratwong <ken.thampiratwong@gmail.com>
2026-02-05 01:51:40 -0800

    dev: restructure frontend

--- frontend/apps/mobile/app/modal.tsx
+++ frontend/apps/mobile/app/modal.tsx
@@ -1,16 +1,12 @@
 import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type='title'>This is a modal</ThemedText>
-      <Link href='/' dismissTo style={styles.link}>
-        <ThemedText type='link'>Go to home screen</ThemedText>
-      </Link>
     </ThemedView>
   );
 }
---
+++ frontend/apps/mobile/src/hooks/index.ts
@@ -0,0 +1,5 @@
+export * from "./use-color-scheme"
+export * from "./use-theme-color"
+
+// FIXME: this should not be here
+export * from "./use-color-scheme.web"
