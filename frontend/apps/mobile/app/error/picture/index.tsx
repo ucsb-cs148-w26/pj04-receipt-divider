2f6557373ff353eaea555901b4666676b86c4a6f
Leifeng Chen <leifengchen@ucsb.edu>
2026-02-19 21:43:58 -0800

    fix: error message much more readable now

--- frontend/apps/mobile/app/error/picture/index.tsx
+++ frontend/apps/mobile/app/error/picture/index.tsx
@@ -1,6 +1,6 @@
 import { router, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Button } from '@eezy-receipt/shared';
 
 export type ErrorPictureParams = {
@@ -12,15 +12,15 @@ export default function PictureErrorScreen() {
   
   return (
     <View style={styles.container}>
-      <View className='flex flex-col rounded-2xl w-[80%] bg-[#ffffff] shadow-xl'>
-        <View className='flex flex-col p-8'>
+      <View className='flex flex-col rounded-2xl w-[80%] bg-[#ffffff] shadow-xl mb-16'>
+        <ScrollView className='flex flex-col p-8 h-[50%]'>
           <Text className='text-2xl font-bold   text-[#374151] pb-6'>
             Receipt Processing Error:
           </Text>
           <Text className='mt-4 text-sm font-medium text-gray-700'>
             Error details: {params.message}
           </Text>
-        </View>
+        </ScrollView>
       </View>
       <Button
         onPress={() => {
