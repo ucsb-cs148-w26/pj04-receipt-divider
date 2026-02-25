39654abb295a442f829317a7543bcfe30a20f230
Leifeng Chen <leifengchen@ucsb.edu>
2026-02-25 08:53:51 -0800

    feat: add IconButton shared component

--- frontend/shared/src/components/IconButton.tsx
+++ frontend/shared/src/components/IconButton.tsx
@@ -1,33 +1,99 @@
-import { IconButton as I } from 'react-native-paper';
+import { Pressable, LayoutChangeEvent } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import type { ComponentProps } from 'react';
-import { COLORS, type ColorName } from '../constants/colors';
+import { useState } from 'react';
 
-export interface IconButtonProps {
+export type IconButtonProps = {
+  /**
+   * Name of the icon from MaterialCommunityIcons. See https://icons.expo.fyi/ with the `MaterialCommunityIcons` filter turned on for available icons.
+   */
   icon: ComponentProps<typeof MaterialCommunityIcons>['name'];
-  iconColor: ColorName;
-  size: ComponentProps<typeof I>['size'];
-  containerColor?: ColorName;
-  mode?: ComponentProps<typeof I>['mode'];
-  onPress?: ComponentProps<typeof I>['onPress'];
-}
+  className?: string;
+  onPress?: () => void;
+  percentageSize?: number;
+  color?: string;
+  /**
+   * `fade`: reduces opacity to 60% when pressed
+   *
+   * `overlay`: adds a gray overlay with 40% opacity when pressed
+   *
+   * `scale`: scales down the button to 92% when pressed
+   */
+  pressEffect?: 'fade' | 'overlay' | 'scale';
+};
 
 export function IconButton({
   icon,
-  iconColor,
-  size,
-  containerColor,
-  mode,
+  className,
   onPress,
+  percentageSize = 60,
+  color = '#000',
+  pressEffect,
 }: IconButtonProps) {
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [pressed, setPressed] = useState(false);
+
+  const fadeStyle = useAnimatedStyle(() => ({
+    opacity: withSpring(pressed ? 0.6 : 1, {
+      damping: 2000, // Higher = slower, less bouncy (default: 10)
+      stiffness: 8000, // Higher = faster, more bouncy (default: 100)
+    }),
+  }));
+  const overlayStyle = useAnimatedStyle(() => ({
+    opacity: withSpring(pressed ? 0.4 : 0, {
+      damping: 2000, // Higher = slower, less bouncy (default: 10)
+      stiffness: 8000, // Higher = faster, more bouncy (default: 100)
+    }),
+  }));
+  const scaleStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: withSpring(pressed ? 0.92 : 1, {
+          damping: 2000, // Higher = slower, less bouncy (default: 10)
+          stiffness: 8000, // Higher = faster, more bouncy (default: 100)
+        }),
+      },
+    ],
+  }));
+
+  const handleLayout = (event: LayoutChangeEvent) => {
+    setContainerWidth(event.nativeEvent.layout.width);
+  };
+
+  const iconSize = (containerWidth * percentageSize) / 100;
+
   return (
-    <I
-      icon={icon}
-      iconColor={COLORS[iconColor]}
-      size={size}
-      containerColor={containerColor ? COLORS[containerColor] : undefined}
-      mode={mode}
-      onPress={onPress}
-    />
+    <Animated.View
+      style={
+        pressEffect === undefined || pressEffect === 'overlay'
+          ? undefined
+          : pressEffect === 'fade'
+            ? fadeStyle
+            : scaleStyle
+      }
+    >
+      <Pressable
+        className={
+          `items-center justify-center rounded-full shadow-md shadow-black/15 ` +
+          className
+        }
+        onLayout={handleLayout}
+        onPressIn={() => setPressed(true)}
+        onPressOut={() => setPressed(false)}
+        onPress={onPress}
+      >
+        <Animated.View
+          style={pressEffect === 'overlay' ? overlayStyle : undefined}
+          className={
+            pressEffect === 'overlay' ? 'bg-gray-500 absolute inset-0' : ''
+          }
+        ></Animated.View>
+        <MaterialCommunityIcons name={icon} size={iconSize} color={color} />
+      </Pressable>
+    </Animated.View>
   );
 }
