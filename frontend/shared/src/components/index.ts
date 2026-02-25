31e1c043e158ae8ebab441e347679715787e56d6
Leifeng Chen <leifengchen@ucsb.edu>
2026-02-24 19:32:00 -0800

    feat: added IconButton using ReactNativePaper

--- frontend/shared/src/components/index.ts
+++ frontend/shared/src/components/index.ts
@@ -3,3 +3,4 @@ export { Participant } from './Participant';
 export { UserTag } from './UserTag';
 export { DisplayItems } from './DisplayItems';
 export { Button } from './Button';
+export { IconButton } from './IconButton';
