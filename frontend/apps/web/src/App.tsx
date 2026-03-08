151bc2336dceee867bec634e3ee19c9fffd41f2d
Roy Lee <roylee@ucsb.edu>
2026-03-08 15:32:42 -0700

    feat/profile-select-page-web

--- frontend/apps/web/src/App.tsx
+++ frontend/apps/web/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router';
 import JoinPage from './pages/JoinPage';
 import GroupPage from './pages/GroupPage';
+import ProfileSelectPage from './pages/ProfileSelectPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<h1>Eezy Receipt</h1>} />
+      <Route path='/profile' element={<ProfileSelectPage />} />
       <Route path='/join' element={<JoinPage />} />
       <Route path='/group/:roomId' element={<GroupPage />} />
       <Route path='*' element={<Navigate to='/' replace />} />
